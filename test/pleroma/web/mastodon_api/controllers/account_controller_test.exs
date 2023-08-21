33f3f8b76966ee5d3c66ea4f422b8d2807dd923b
marcin mikołajczak <git@mkljczk.pl>
2023-08-21 15:06:12 +0200

    Format code
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- test/pleroma/web/mastodon_api/controllers/account_controller_test.exs
+++ test/pleroma/web/mastodon_api/controllers/account_controller_test.exs
@@ -2283,10 +2283,12 @@ defmodule Pleroma.Web.MastodonAPI.AccountControllerTest do
       CommonAPI.follow(other_user, user)
 
       assert %{"id" => ^other_user_id, "followed_by" => false} =
-      assert %{"id" => ^other_user_id, "followed_by" => false} =
-               conn
-               |> post("/api/v1/accounts/#{other_user_id}/remove_from_followers")
-               |> json_response_and_validate_schema(200)
+               assert(
+                 %{"id" => ^other_user_id, "followed_by" => false} =
+                   conn
+                   |> post("/api/v1/accounts/#{other_user_id}/remove_from_followers")
+                   |> json_response_and_validate_schema(200)
+               )
 
       refute User.following?(other_user, user)
     end
